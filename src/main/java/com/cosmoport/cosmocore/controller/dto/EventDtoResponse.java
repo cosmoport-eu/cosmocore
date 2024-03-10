a3d7cb9916a91e920f3188a22105fbf874e61bf9
Mark <728239@gmail.com>
2024-03-10 09:54:41 +0100

    #6: фикс NPE

--- src/main/java/com/cosmoport/cosmocore/controller/dto/EventDtoResponse.java
+++ src/main/java/com/cosmoport/cosmocore/controller/dto/EventDtoResponse.java
@@ -1,7 +1,7 @@
 package com.cosmoport.cosmocore.controller.dto;
 
 public record EventDtoResponse(int id, String eventDate, int eventTypeId, int eventStateId,
-                               int eventStatusId, int gateId, int gate2Id, int startTime, int durationTime,
+                               int eventStatusId, Integer gateId, Integer gate2Id, int startTime, int durationTime,
                                int repeatInterval,  double cost, int peopleLimit, int contestants, String dateAdded
 ) {
 }
